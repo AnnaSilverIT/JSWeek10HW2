dbac401ba3fbe382cbed70d3cf747e123a087930
AnnaSilverIT <student291297@gmail.com>
2024-08-18 19:20:40 +0300

    feat html css and js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let image = document.getElementById("catImage");
+function changeImage1() {
+    image.src="./assets/img/cat1.jpg"
+}
+function changeImage2() {
+  image.src="./assets/img/cat2.jpg"
+}
